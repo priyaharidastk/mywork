8df2b09af035537af49419b3ee1936b9936cfeb5
priyaharidastk <88211667+priyaharidastk@users.noreply.github.com>
2021-07-31 01:42:33 +0530

    Add files via upload

---
+++ pk.py
@@ -0,0 +1,26 @@
+print("hello")
+name="krishna priya@"
+print (name)
+x=3
+y=3
+sum=x+y
+print(sum)
+x="3"
+y="3"
+sum1=x+y
+print(sum1)
+x="3"
+y="3"
+sum1=x+y
+print(sum1)
+x = str(3)    
+print (x)
+y = int("45")  
+print(y) 
+z = float("45") 
+print(z) 
+def myfunction(myname):
+    print ("hello "+myname)
+
+myfunction("krishnapriya")
+
